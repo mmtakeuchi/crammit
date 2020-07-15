bb46cdaca0cd09866218c3498d8f3cd9f3817405
mmtakeuchi <mikemtaki@gmail.com>
2020-07-14 21:35:48 -0700

    create helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ class UsersController < ApplicationController
     get '/signup' do
         erb :"/users/signup"
     end
+
+    post '/signup' do
+        @user = User.new(params[:user])
+        if @user.save
+            redirect '/'
+        else
+            redirect '/signup'
+        end
+    end
 end
